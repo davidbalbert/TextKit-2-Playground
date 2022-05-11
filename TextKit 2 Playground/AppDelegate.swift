2ddfa437276c14a99693dc0d669137acb38978f9
David Albert <davidbalbert@gmail.com>
2022-05-11 07:16:00 -0400

    Initial Commit

---
+++ TextKit 2 Playground/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TextKit 2 Playground
+//
+//  Created by David Albert on 5/11/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ TextKit 2 PlaygroundUITests/TextKit_2_PlaygroundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TextKit_2_PlaygroundUITestsLaunchTests.swift
+//  TextKit 2 PlaygroundUITests
+//
+//  Created by David Albert on 5/11/22.
+//
+
+import XCTest
+
+class TextKit_2_PlaygroundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
