a68db4153d618d04aa14fdc4d9fb3b4419acb849
David Albert <davidbalbert@gmail.com>
2022-06-11 22:17:31 -0400

    Add note

--- TextKit 2 Playground/TextView+TextInput.swift
+++ TextKit 2 Playground/TextView+TextInput.swift
@@ -21,6 +21,9 @@ extension TextView: NSTextInputClient {
 
     override func doCommand(by selector: Selector) {
         if responds(to: selector) {
+            // All these methods return void. Is takeUnretainedValue right here?
+            // Do I need it? I got a crash when I used perform(_:) without an
+            // argument.
             _ = perform(selector, with: nil).takeUnretainedValue()
         } else {
             print("doCommandBySelector:", selector)
